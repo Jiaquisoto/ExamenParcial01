432c0aab6dd5bd30617b1ba872d17e3b899aecfb
UPEU <UPEU@172.17.1.10>
2017-04-26 21:57:51 -0500

    v1:versión 1 del examen 01

--- ExamenG1/public_html/js/scripts.js
+++ ExamenG1/public_html/js/scripts.js
@@ -1,7 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+function ver(home) {
+         document.getElementById("subseccion"+home).style.display="block"
+         }
+function ocultar(about) {
+         document.getElementById("subseccion"+blog).style.display="none"
+         }
+function ocultar(blog) {
+         document.getElementById("subseccion"+blog).style.display="none"
+         } 
+function ocultar(contact) {
+         document.getElementById("subseccion"+blog).style.display="none"
+         }
+         
+         
 
 
